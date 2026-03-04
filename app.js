8b9b58c869e9b3a10ae5c6f6f07dcc451d16e970
Bomiyaji <134933802+Bomiyaji@users.noreply.github.com>
2026-03-04 09:34:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,41 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
-import {
-getDatabase,
-ref,
-push,
-onChildAdded
-} from "https://www.gstatic.com/firebasejs/12.10.0/firebase-database.js";
+import { getDatabase, ref, push, onChildAdded }
+from "https://www.gstatic.com/firebasejs/12.10.0/firebase-database.js";
 
-const firebaseConfig = {
-apiKey: "YOUR_API_KEY",
-authDomain: "chatroom5757.firebaseapp.com",
-databaseURL:"https://chatroom5757-default-rtdb.firebaseio.com",
-projectId: "chatroom5757",
+const firebaseConfig={
+ apiKey:"AIzaSyCLHlwvG9Cpu5D7ssztKDQEolB4iV-0-FY",
+ authDomain:"chatroom5757.firebaseapp.com",
+ databaseURL:"https://chatroom5757-default-rtdb.firebaseio.com",
+ projectId:"chatroom5757"
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
+const app=initializeApp(firebaseConfig);
+const db=getDatabase(app);
+
+const chatRef=ref(db,"romanticChat");
 
 let username="";
-const chatRef = ref(db,"liveChat");
 
 window.joinChat=function(){
+ username=document.getElementById("username").value;
+ if(!username) return alert("Enter name");
 
-username=document.getElementById("username").value;
-
-if(!username) return alert("Enter name");
-
-document.getElementById("joinScreen").style.display="none";
-document.getElementById("chatScreen").classList.remove("hidden");
+ document.getElementById("join").style.display="none";
+ document.getElementById("chat").classList.remove("hidden");
 };
 
 window.sendMessage=function(){
 
-const text=document.getElementById("msgInput").value;
-if(text==="") return;
+const input=document.getElementById("msgInput");
+if(!input.value) return;
 
 push(chatRef,{
-name:username,
-message:text
+ name:username,
+ message:input.value,
+ time:new Date().toLocaleTimeString()
 });
 
-document.getElementById("msgInput").value="";
+input.value="";
 };
 
 onChildAdded(chatRef,(data)=>{
@@ -49,11 +45,13 @@ const msg=data.val();
 const div=document.createElement("div");
 div.className="msg "+(msg.name===username?"me":"other");
 
-div.innerText=msg.name+": "+msg.message;
+div.innerHTML=
+`<b>${msg.name}</b><br>
+${msg.message}
+<div class="time">${msg.time}</div>`;
 
 document.getElementById("messages").appendChild(div);
 
 document.getElementById("messages").scrollTop=
 document.getElementById("messages").scrollHeight;
-
 });
